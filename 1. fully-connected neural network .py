60a05a190607c8675ae765ecf348aacf1d463b69
sine-x0x <tls_99@yonsei.ac.kr>
2020-10-03 00:14:54 +0900

    1.full NN upload

---
+++ 1. fully-connected neural network .py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# title : fully-connected neural network & loss drop
+
+# 상황 )
+#  - one input layer(size 2), one hidden layer(size 8), one output layer(size 1)
+#  - learning rate : 0.01
+#  - stochastic gradient descent : weight update ( batch size = 1 )
+#  - plot the loss versus the number of epochs => convergence of training
+
+# 1. set the environment
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+train = np.loadtxt('training.txt')
+X = train[:,0:2] 
+t = train[:,2:3] #true
+p = np.zeros(1000) #predict
+
+#  2. define link function as sigmoid
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def dsigmoid(x):   # 'word2vec' p17
+    return x*(1-x)
+
+# 3. update equation
+def fullNN(n_epochs,lr,input,hidden,output):
+    np.random.seed(2020)
+    w = np.random.randn(input,hidden) # 2*8
+    w_prime = np.random.randn(hidden,output) #8*1
+    
+    lossdata = 0 ; lossarray = []
+    
+    for epoch in range(n_epochs):
+        for i in range(1000):
+            pick = np.random.choice(1000, size = 1, replace = False ) #random batch
+            
+            x = X[pick] # of input layer
+            u = np.dot(x,w) # net input of hidden layer
+            
+            h = sigmoid(u) # hidden layer
+            u_prime = np.dot(h,w_prime) # net output of hidden layer
+            
+            p = sigmoid(u_prime) # output layer
+            
+            loss = 0.5*((p-t[pick])**2)
+            
+            """
+            chain rule따라 쭉쭉 gradient 계산
+            """
+            dL_y = p-t[pick]
+            dL_uprime = dL_y*dsigmoid(p)
+            dL_h = np.dot(dL_uprime,w_prime.T) #.T : transpose
+            dL_u = dL_h*dsigmoid(h)
+            dL_w = np.dot(np.transpose(x),dL_u)
+            dL_w_prime = np.dot(np.transpose(h),dL_uprime)
+                      
+            w = w - lr*dL_w
+            w_prime = w_prime - lr*dL_w_prime
+            lossdata = lossdata + np.sum(loss)
+            
+        if epoch%100==0:
+            print("epoch : "+ str(epoch))
+            
+        lossarray.append(lossdata)
+        lossdata = 0
+    return lossarray
+
+# 4. run
+n_epochs = 500 ; lr = 0.005 ; input = 2 ; hidden = 8 ; output = 1
+lossarray = fullNN(n_epochs,lr,input,hidden,output)
+
+# 5. output
+plt.plot(range(500),lossarray)
+plt.title('loss per epoch number in SGD')
+plt.xlabel('the number of epoch')
+plt.ylabel('Loss')
+plt.show()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# paper-embedding
+﻿# paper-embedding
 논문 구현을 통한 알고리즘 공부 [CSE URP 23기]
+
+### Papers 
+
+1) B. Perozzi, R. Al-Rfou, and S. Skiena, “DeepWalk: Online learning of social representations,” in Proc. KDD’14, New York, NY USA.
+2) J. Tang, M. Qu, M. Wang, M. Zhang, J. Yan, and Q. Mei, “LINE: Large-scale information network embedding,” in Proc. WWW’15, Florence, Italy.
+3) A. Grover and J. Leskovec, “node2vec: Scalable feature learning for networks,” in Proc. KDD’16, San Francisco, CA USA.
